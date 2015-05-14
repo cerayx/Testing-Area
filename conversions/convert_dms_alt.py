d5e5198dd299e9a042936174483a873ffe8cd43c
cerayx <cerayx@gmail.com>
2015-05-14 16:46:10 -0500

    working solution, needs futher refining

--- conversions/convert_dms_alt.py
+++ conversions/convert_dms_alt.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
 import math
-'''
-Given a Degrees Decimal Minutes coordinate such as -90°03.11202W, convert it to a number of decimal degrees using the following method:
-    (1) The integer number of degrees is the same (90)
-    (2) The decimal degrees is the decimal minutes divided by 60 (03.11202/60 = ~ .051867)
-    (3) Sum of the two numbers 90 + .051867 = 90.051867)
-    (4) For coordinates in the western or southern hemispheres, negate the result
-    (5) The final result is -90.051867
-
-from left to right in string: 
-    (1) convert leading zero hemisphere indicator to a negative sign (-)
-    (2) convert first instance of whitespace to the degree sign (°)
-    (3) remove any leading zero after whitespace
-    (4) truncate/round int after decimal to three significant digits
-    (5) convert directional to an apostrophe sign (') '''
-
-def main(x):
-    decimal, degree = math.modf(x[1])
-    y = abs(round((decimal*60), 3))
+import pprint
 
 
-    print "{}° {}'".format(int(degree),y)
+class MyPrettyPrinter(pprint.PrettyPrinter):
+
+    def format(self, object, context, maxlevels, level):
+        if isinstance(object, unicode):
+            return (object.encode('utf8'), True, False)
+        return pprint.PrettyPrinter.format(self, object, context,
+                                           maxlevels, level)
+
+
+def main(element):
+    decimal, degree = math.modf(element)
+    decimal_min = abs(round((decimal * 60), 3))
+    sign = u"\u00B0"
+    return u"{}{} {}'".format(int(degree), sign, decimal_min)
+
 
 if __name__ == '__main__':
     coordinates = ((35.1559396703, -90.051861154),
@@ -32,4 +29,6 @@ if __name__ == '__main__':
                    (35.1345278453, -90.0575200378),
                    (35.0979580489, -89.8516395905))
     for i in coordinates:
-        main(i)
+        print(map(main, i))
+        MyPrettyPrinter().pprint(map(main, i))
+        # verify output
